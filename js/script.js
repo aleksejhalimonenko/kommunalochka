2d762f9e082ed930b9f0d10ebf457ccd99777086
aleksejhalimonenko <155500108+aleksejhalimonenko@users.noreply.github.com>
2025-05-20 23:34:11 +0200

    Update script.js
    full scr

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
 	window.Telegram.WebApp.expand();  // Разворачиваем WebApp на весь экран
 	window.Telegram.WebApp.MainButton.setText("Отправить данные");
 	window.Telegram.WebApp.MainButton.show();
+	window.Telegram.WebApp.MainButton.requestFullscreen();
   }
 
 document.documentElement.requestFullscreen(); 
